f951c7ad70fa7ca030f9cb5205b9fa7902856737
Piyawat AP. Assanont <tonporza01@gmail.com>
2024-08-11 16:28:09 +0700

    set library
    set library MUI / Tailwind / DaisyUI / Router

--- FinalProject_BTD/src/App.tsx
+++ FinalProject_BTD/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="flex flex-row gap-4 justify-center align-items-center">
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Final Project BTD FED# {count}</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button className='text-2xl text-yellow-400' onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
